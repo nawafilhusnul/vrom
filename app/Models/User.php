b8b3e903de582bc5aec3dca1b8fe8bc2a4ee82a5
nawafilhusnul <nawafilhusnul@gmail.com>
2024-01-01 23:32:00 +0700

    fix: adjust user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,9 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function bookings(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Booking::class);
+    }
 }
