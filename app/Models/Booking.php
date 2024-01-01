0ff1d075a6131bf9671ced604783025f7a9b58f9
nawafilhusnul <nawafilhusnul@gmail.com>
2024-01-01 23:31:43 +0700

    feat: add booking model

---
+++ app/Models/Booking.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Booking extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'start_date',
+        'end_date',
+        'address',
+        'city',
+        'zip',
+        'status',
+        'payment_method',
+        'payment_status',
+        'payment_url',
+        'total_price',
+        'item_id',
+        'user_id',
+    ];
+
+    public function item(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Item::class);
+    }
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
